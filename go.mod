13772a6c9dd36f9df2c2e54b7650319704db6a50
juanmontilva <montilva.juancarlos@gmail.com>
2024-05-24 01:49:07 -0400

    siguiente commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/juanmontilva/xgo
 
 go 1.22.3
+
+require github.com/aws/aws-lambda-go v1.47.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"context"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	lambda "github.com/aws/aws-lambda-go/lambda"
+)
+
 func main() {
+	lambda.Start(EjecutoLambda)
+}
+
+func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	var res *events.APIGatewayProxyResponse
+
+	if !ValidoParametro() {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Error en las variables de entorno. debe incluir 'SecretName', 'BucketName', 'UrlPrefix'",
+			Headers: map[string]string{
+				"Content-Type": "aplication/json",
+			},
+		}
+	}
+
+	return res, nil
+
+}
+
+func ValidoParametro() bool {
+	_, traeParametro := os.LookupEnv("SecretName")
+	if !traeParametro {
+		return traeParametro
+	}
+
+	_, traeParametro = os.LookupEnv("BucketName")
+	if !traeParametro {
+		return traeParametro
+	}
+
+	_, traeParametro = os.LookupEnv("UrlPrefix")
+	if !traeParametro {
+		return traeParametro
+	}
+
+	return traeParametro
 
 }
